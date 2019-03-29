def8a7d920e2c3298990038fcb6b8c29d3608b70
wungmc <wungcc@gmail.com>
2019-03-29 15:02:20 +0800

    helloworld

---
+++ frontend/src/views/test/Test.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    Hello <span v-if="a">world</span>
+    <a-button type="dashed" @click="handleclick">aniu</a-button>
+    <a-time-picker use12Hours />
+    <a-button type="danger">danger12</a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    function data() {
+      return {
+        a: 1
+      }
+    }
+    return {
+      a: true,
+      b: "",
+      c: {}
+    }
+  },
+  methods: {
+    handleclick: function () {
+      this.a = !this.a
+    }
+
+  }
+}
+
+</script>
