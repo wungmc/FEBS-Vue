8df39dd054a562e0a680cf431c723975f2f6491a
wungmc <wungcc@gmail.com>
2019-03-29 15:03:32 +0800

    cdn.bootcss.com 换成 cdnjs.cloudflare.com

--- frontend/src/utils/color.js
+++ frontend/src/utils/color.js
@@ -40,7 +40,8 @@ const updateTheme = primaryColor => {
         javascriptEnabled: true
       }
     `
-    lessScriptNode.src = 'https://cdn.bootcss.com/less.js/3.9.0/less.min.js'
+    // lessScriptNode.src = 'https://cdn.bootcss.com/less.js/3.9.0/less.min.js'
+    lessScriptNode.src = 'https://cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js'
     lessScriptNode.async = true
     lessScriptNode.onload = () => {
       buildIt()
